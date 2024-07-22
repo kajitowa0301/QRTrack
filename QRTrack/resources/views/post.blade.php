f5b3efda9a07794011e2a30a129ca643aa8d01d1
zumi <2301902@s.asojuku.ac.jp>
2024-07-22 09:45:43 +0900

    post画面

---
+++ QRTrack/resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel</title>
+
+  @vite(['resources/css/app.css','resources/js/app.js']) <!-- ←これを書き込むことでTaileindCSSが導入できる -->
+</head>
+  <body class="w-full h-full md:gra sm:bg-red-200 bg-blue-200">
+    <x-header-component />
+    <div class="">
+    
+
+
+
+    </div>
+  </body>
+
+</html>
