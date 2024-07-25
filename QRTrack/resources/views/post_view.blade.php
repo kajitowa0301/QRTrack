62095e9d5bc8dce22b6f2e0a6494e4f9ee3adecd
hanabonbon <107159047+hanabonbon@users.noreply.github.com>
2024-07-25 11:59:58 +0900

    投稿閲覧画面ファイル作成
    投稿閲覧画面のファイルを作成しました

---
+++ QRTrack/resources/views/post_view.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>投稿閲覧画面</title>
+
+  @vite(['resources/css/app.css','resources/js/app.js']) <!-- ←これを書き込むことでTaileindCSSが導入できる -->
+</head>
+  <body class="">
+
+
+    </div>
+  </body>
+
+</html>
