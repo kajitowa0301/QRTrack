0b5917c73dde74e5c0b962c35b5d4680ff0ce53b
zumi <2301902@s.asojuku.ac.jp>
2024-07-29 07:37:55 +0900

    post更新

--- QRTrack/resources/views/components/textbox-component.blade.php
+++ QRTrack/resources/views/components/textbox-component.blade.php
@@ -10,77 +10,63 @@
     </div>
 </div> -->
 <x-guest-layout>
+<div class = "w-full">
+    <div class="w-full grid grid-cols-2 grid-rows-1">
+        <div class="w-full col-span-1 row-span-2">
+            <div class ="text-gray-500">作成者</div>
+            <div class ="">ゲスト</div>
+        </div>
+        <div class="col-span-1 row-span-2">
+            <div class ="text-gray-500">作成日</div>
+            <div class ="">2024/07/29</div>
+        </div>
+
+
+    </div>
+</div>
+
   <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- ものの名前 -->
-        <div>
+        <div class = "mt-8">
             <x-input-label for="users_mail" :value="__('モノの名前')" />
             <x-text-input id="users_mail" class="block mt-1 w-full" type="text" name="users_mail" :value="old('email')" required
-                autofocus autocomplete="username" />
+                autofocus  />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
+        <!-- タイトル -->
+        <div class="mt-8">
             <x-input-label for="password" :value="__('タイトル')" />
 
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="text" name="password" required
+                 />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
     <!-- Input -->
-<div class="relative">
-  <textarea class="p-4 pb-12 block w-full bg-gray-100 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Ask me anything..."></textarea>
-
-  <!-- Toolbar -->
-  <div class="absolute bottom-px inset-x-px p-2 rounded-b-lg bg-gray-100 dark:bg-neutral-800">
-    <div class="flex justify-between items-center">
-      <!-- Button Group -->
-      <div class="flex items-center">
-        <!-- Mic Button -->
-        <button type="button" class="inline-flex shrink-0 justify-center items-center size-8 rounded-lg text-gray-500 hover:bg-white focus:z-10 focus:outline-none focus:bg-white dark:text-neutral-500 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-          <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="3" rx="2"/><line x1="9" x2="15" y1="15" y2="9"/></svg>
-        </button>
-        <!-- End Mic Button -->
-
-        <!-- Attach Button -->
-        <button type="button" class="inline-flex shrink-0 justify-center items-center size-8 rounded-lg text-gray-500 hover:bg-white focus:z-10 focus:outline-none focus:bg-white dark:text-neutral-500 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-          <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48"/></svg>
-        </button>
-        <!-- End Attach Button -->
+      <div class="relative mt-8">
+      <x-input-label for="password" :value="__('詳細情報')" />
+        <textarea class="mt-1 p-4 pb-12 block w-full border-gray-300 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="" required></textarea>
       </div>
-      <!-- End Button Group -->
+    <!-- End Input -->
 
-      <!-- Button Group -->
-      <div class="flex items-center gap-x-1">
-        <!-- Mic Button -->
-        <button type="button" class="inline-flex shrink-0 justify-center items-center size-8 rounded-lg text-gray-500 hover:bg-white focus:z-10 focus:outline-none focus:bg-white dark:text-neutral-500 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-          <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/><line x1="12" x2="12" y1="19" y2="22"/></svg>
-        </button>
-        <!-- End Mic Button -->
-
-        <!-- Send Button -->
-        <button type="button" class="inline-flex shrink-0 justify-center items-center size-8 rounded-lg text-white bg-blue-600 hover:bg-blue-500 focus:z-10 focus:outline-none focus:bg-blue-500">
-          <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
+    <!-- ボタン -->
+    <div class="flex justify-center mt-8">
+      <button class="group flex h-18 w-18 select-none items-center justify-center rounded-full border border-black border-zinc-100 bg-white leading-8 text-zinc-950 shadow-[0-1px_0_0px#d4d4d8inset,0_0_0_1px#f4f4f5inset,0_0.5px_0_1.5px#fffinset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px#e4e4e7inset,1px_0px_1px_0px#e4e4e7inset,0px_0.125rem_1px_0px#d4d4d8_inset]" aria-label="Change language" style="height: 4.5rem; width: 4.5rem;">
+        <span class="flex items-center justify-center group-active:[transform:translate3d(0,1px,0)]">
+          <svg width="24" height="24" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-zinc-950">
+            <path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
+            fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
           </svg>
-        </button>
-        <!-- End Send Button -->
-      </div>
-      <!-- End Button Group -->
+        </span>
+      </button>
     </div>
-  </div>
-  <!-- End Toolbar -->
-</div>
-<!-- End Input -->
 
 
 
     </form>
-
-
 
 </x-guest-layout>
