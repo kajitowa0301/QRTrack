ba2052f817952da0e105ca1632c4201fbecd1f51
zumi <2301902@s.asojuku.ac.jp>
2024-10-31 09:51:03 +0900

    検索画面にも検索バーを追加(仮)

--- QRTrack/resources/views/search_view.blade.php
+++ QRTrack/resources/views/search_view.blade.php
@@ -11,6 +11,11 @@
 </head>
 <body class="w-full">
   <x-header-component />
+  <form method = "POST" action = "{{ route('search') }}" class="my-4 flex justify-center">
+    @csrf
+      <input type = "text" name = "keyword" class="border-2 border-gray-400 rounded-lg w-9/12 mr-10" placeholder="🔍キーワードを入力">
+      <button type="submit" class="bg-blue-200 text-black p-2 rounded ml-2">検索</button>
+  </form>
   <div class="px-4 sm:px-6 md:px-8 lg:px10 xl:px-12 mx-auto mt-4">
     <!-- 検索キーワードを表示 -->
     <h1 class="text-2xl font-bold mb-4">検索結果: "{{ $query }}"</h1> 
