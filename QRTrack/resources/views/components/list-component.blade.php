9d076e419a8151440e520dadd736bbc84dd17b11
zumi <2301902@s.asojuku.ac.jp>
2024-10-31 13:10:06 +0900

    追加機能更新
    動作はするが表示されない

--- QRTrack/resources/views/components/list-component.blade.php
+++ QRTrack/resources/views/components/list-component.blade.php
@@ -1,13 +1,13 @@
 <div class="w-full ">
     <!-- 大枠 -->
     <div class="mx-4 outline border-gray-400 outline-1 rounded-xl">
-        <!-- タイトル -->
-        <div class=" flex justify-center items-center mt-4 text-xl font-medium">
-            <p>{{$details->first()->details_title}}</p>
-        </div>
-        <!-- 詳細情報 -->
-        <div class="flex justify-center items-center text-l  text-black  font-normal">
-            <p class="m-5">{{$details->first()->details_content}}</p>
-        </div>
+            <!-- タイトル -->
+            <div class="flex justify-center items-center mt-4 text-xl font-medium">
+                <p>{{ $detail->details_title }}</p>
+            </div>
+            <!-- 詳細情報 -->
+            <div class="flex justify-center items-center text-l text-black font-normal">
+                <p class="m-5">{{ $detail->details_content }}</p>
+            </div>
     </div>
 </div>
--- QRTrack/resources/views/post_view.blade.php
+++ QRTrack/resources/views/post_view.blade.php
@@ -17,13 +17,12 @@
   <div class="flex justify-center items-center">
     <p class=" font-bold text-3xl">{{ $postsType }}</p>
   </div>
-  <x-list-component :$postId/>
   @foreach ($datas as $detail)
     <div class="mt-4">
-      <h3 class="text-lg font-bold text-gray-800">{{ $detail->details_title }}</h3>
-      <p class="mt-1 text-gray-500">{{ $detail->details_content }}</p>
+    <x-list-component :detail="$detail"/>
     </div>
   @endforeach
+
   @if ($usersId == Auth::id())
     <div class="w-1/3 flex justify-evenly">
       <a href="{{ route('postAddDetailForm', ['id' => $postId]) }}">
