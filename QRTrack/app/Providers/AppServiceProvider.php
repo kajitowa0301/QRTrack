002b7f148f87a9fc0a4f9a3d2ac9c83f6122fe41
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 18:38:43 +0900

    Update AppServiceProvider.php

--- QRTrack/app/Providers/AppServiceProvider.php
+++ QRTrack/app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use App\View\Components\cardComponent;
+use App\View\Components\deleteBox;
+use App\View\Components\likeButtonComponent;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use App\View\Components\listComponent;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Blade::component('components.card-component', cardComponent::class);
         Blade::component('componets.list-component', listComponent::class);
+        Blade::component('components.like-button-component', likeButtonComponent::class);
+        Blade::component('components.delete-box-component', deleteBox::class);
     }
 }
