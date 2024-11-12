751697731d7367eb6df9da71ffd238e8a9e67b61
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 16:54:48 +0900

    tesy

--- QRTrack/app/Http/Controllers/ViewController.php
+++ QRTrack/app/Http/Controllers/ViewController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Posts;
+use Illuminate\Support\Facades\DB;
 
 class ViewController extends Controller
 {
-    public function home(){
-        $datas = Posts::get('posts_id');
-        return view('home',compact('datas'));
+    public function home()
+    {
+        // $datas = Posts::get('posts_id');
+        $datas = DB::table('posts')
+            ->join('post_details', 'posts.posts_id', '=', 'post_details.posts_id')
+            ->select(
+                'posts.posts_id',
+                'posts.posts_type',
+                'posts.img_path',
+                'posts.posts_qr',
+                'post_details.title',
+                'post_details.content'
+            )
+            ->get();
+
+          return view('home',compact('datas'));
+
     }
 }
