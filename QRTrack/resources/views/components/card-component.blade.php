4c0f6b7d318d820958af4974f2b8e53b9132eda1
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 18:29:57 +0900

    Update card-component.blade.php

--- QRTrack/resources/views/components/card-component.blade.php
+++ QRTrack/resources/views/components/card-component.blade.php
@@ -18,11 +18,11 @@
     </a>
     <div class=" flex justify-between">
         <div class="pl-1 w-1/2">
-            <x-like-button-component :$id = "$data->posts_id" />
+            <x-like-button-component :$id="$data->posts_id" />
         </div>
         @if ($data->users_id == Auth::id())
             <div class="w-1/3 flex justify-evenly">
-                <x-delete-box :$id = "$data->posts_id" />
+                <x-delete-box :$id="$data->posts_id" />
             </div>
         @endif
     </div>
