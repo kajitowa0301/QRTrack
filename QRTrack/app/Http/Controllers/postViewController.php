c49715a9a0652af6163996dba8e231ca31c854fd
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 17:42:20 +0900

    Update postViewController.php

--- QRTrack/app/Http/Controllers/postViewController.php
+++ QRTrack/app/Http/Controllers/postViewController.php
@@ -13,7 +13,7 @@ class postViewController extends Controller
         // $idを使ってPostsモデルからデータを取得
         $postData = Posts::find($id);
         $datas = Posts::where('posts_id', $id)->get();
-
+        dd($postData);
         return view('post_view', compact('postData', 'datas' ,'id'));
     }
 }
