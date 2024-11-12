83bfe65e7cbda11c24571b023c64f33514577ab3
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 10:40:26 +0900

    テスト

--- QRTrack/app/View/Components/cardComponent.php
+++ QRTrack/app/View/Components/cardComponent.php
@@ -23,9 +23,11 @@ class cardComponent extends Component
     public function render(): View|Closure|string
     {
         $data = $this->data;
+        dd($data);
         $id = $data['posts_id'];
+        dd($id);
         $postData = Posts::where('posts_id', $id)->first(['users_id','posts_type','posts_qr','img_path']);
-	$title_content = PostDetails::where('posts_id', $id)->first();
+	    $title_content = PostDetails::where('posts_id', $id)->first();
         return view('components.card-component', compact('postData', 'title_content', 'id'));
     }
 }
