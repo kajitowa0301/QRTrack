60b6ff0cbf13f3fd0c9d381e3b2a996bbe855c6d
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 23:43:09 +0900

    Update ViewController.php

--- QRTrack/app/Http/Controllers/ViewController.php
+++ QRTrack/app/Http/Controllers/ViewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\PostDetails;
 use Illuminate\Http\Request;
 use App\Models\Posts;
 use Illuminate\Support\Facades\DB;
@@ -12,21 +13,11 @@ class ViewController extends Controller
     public function home()
     {
         // $datas = Posts::get('posts_id');
-        $datas = DB::table('posts')
-        ->leftJoin('post_details', 'posts.posts_id', '=', 'post_details.posts_id')
-        ->select(
-            'posts.posts_id',
-            'posts.users_id',
-            'posts.posts_type',
-            'posts.img_path',
-            'posts.posts_qr',
-            DB::raw('GROUP_CONCAT(post_details.details_title SEPARATOR "||") as details_titles'),
-            DB::raw('GROUP_CONCAT(post_details.details_content SEPARATOR "||") as details_contents')
-        )
-        ->groupBy('posts.posts_id', 'posts.users_id', 'posts.posts_type', 'posts.img_path', 'posts.posts_qr')
-        ->get();
-                dd($datas);
-          return view('home',compact('datas'));
+        $datas = Posts::get(['posts_id','posts_type','posts_qr','img_path']);
+        $details = PostDetails::get([,'details_title','details_content']);
+        $zipped = collect($datas)->zip($details);
+        dd($zipped);
+        return view('home',compact('datas'));
 
     }
 }
