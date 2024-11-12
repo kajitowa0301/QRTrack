2d1420c77d094c051656e60373b6f84403a9bc21
永遠 <2101026@s.asojuku.ac.jp>
2024-11-12 23:44:58 +0900

    Update ViewController.php

--- QRTrack/app/Http/Controllers/ViewController.php
+++ QRTrack/app/Http/Controllers/ViewController.php
@@ -14,7 +14,7 @@ class ViewController extends Controller
     {
         // $datas = Posts::get('posts_id');
         $datas = Posts::get(['posts_id','posts_type','posts_qr','img_path']);
-        $details = PostDetails::get([,'details_title','details_content']);
+        $details = PostDetails::get(['details_title','details_content']);
         $zipped = collect($datas)->zip($details);
         dd($zipped);
         return view('home',compact('datas'));
