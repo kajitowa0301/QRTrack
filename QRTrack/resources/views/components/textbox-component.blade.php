6ad6e9906e526649d258873454c175179b193186
永遠 <2101026@s.asojuku.ac.jp>
2024-11-13 11:40:17 +0900

    Update textbox-component.blade.php

--- QRTrack/resources/views/components/textbox-component.blade.php
+++ QRTrack/resources/views/components/textbox-component.blade.php
@@ -21,6 +21,6 @@
 <!-- 詳細情報 -->
 <div class="relative mt-8">
   <x-input-label for="password" :value="__('詳細情報')" />
-  <textarea id = "postdetail" name = "details_content" class="mt-1 p-4 pb-12 block w-full border-gray-300 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="" required></textarea>
+  <textarea id = "postdetail" name = "details_content" class="mt-1 p-4 pb-12 block w-full border-gray-300 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500" placeholder="" required></textarea>
 </div>
 <!-- End 詳細情報 -->
