4ab61418d7ba04310ab71395b98c28791f972d81
永遠 <2101026@s.asojuku.ac.jp>
2024-11-13 10:09:06 +0900

    Update ViewController.php

--- QRTrack/app/Http/Controllers/ViewController.php
+++ QRTrack/app/Http/Controllers/ViewController.php
@@ -12,10 +12,8 @@ class ViewController extends Controller
     public function home()
     {
         // $datas = Posts::get('posts_id');
-        $datas = Posts::with(['details' => function ($query) {
-            $query->orderBy('posts_id')->limit(1);
-        }])->get();
-        dd($datas->details[0]);
+        $datas = Posts::with('details')->get();
+        dd($datas);
           return view('home',compact('datas'));
 
     }
