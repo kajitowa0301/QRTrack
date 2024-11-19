7b9bb8030a7f0cf74dd6090e265f84a6c62bbfa0
永遠 <2101026@s.asojuku.ac.jp>
2024-11-19 17:37:29 +0900

    修正

--- QRTrack/resources/views/layouts/guest.blade.php
+++ QRTrack/resources/views/layouts/guest.blade.php
@@ -13,8 +13,10 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="icon" href="{{asset('img/favicon.ico')}}">
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        <x-header-component />
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <!-- <div>
                 <a href="/">
