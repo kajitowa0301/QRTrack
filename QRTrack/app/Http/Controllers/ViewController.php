91822683f3d1a6ada4cc6507fe074295d7187949
永遠 <2101026@s.asojuku.ac.jp>
2024-11-20 10:32:29 +0900

    push

--- QRTrack/app/Http/Controllers/ViewController.php
+++ QRTrack/app/Http/Controllers/ViewController.php
@@ -19,7 +19,7 @@ class ViewController extends Controller
             ORDER BY details_id ASC 
             LIMIT 1
         ) as details_title'))
-        ->get()::paginate(2);
+        ->paginate(2);
           return view('home',compact('datas'));
 
     }
