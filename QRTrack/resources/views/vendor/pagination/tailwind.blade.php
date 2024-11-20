1a27178005c23544cca940d9c0f235d22f89da70
永遠 <2101026@s.asojuku.ac.jp>
2024-11-20 11:50:44 +0900

    Update tailwind.blade.php

--- QRTrack/resources/views/vendor/pagination/tailwind.blade.php
+++ QRTrack/resources/views/vendor/pagination/tailwind.blade.php
@@ -60,12 +60,12 @@
 
                     {{-- Pagination Elements --}}
                     @foreach ($elements as $element)
-                        {{-- "Three Dots" Separator --}}
+                        <!-- {{-- "Three Dots" Separator --}}
                         @if (is_string($element))
                             <span aria-disabled="true">
                                 <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5 dark:bg-gray-800 dark:border-gray-600">{{ $element }}</span>
                             </span>
-                        @endif
+                        @endif -->
 
                         {{-- Array Of Links --}}
                         @if (is_array($element))
